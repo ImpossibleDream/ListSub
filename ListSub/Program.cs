5e078699489fe8293bd7bfd31c31473c79b72022
Fan Jing <jing1999fan@outlook.com>
2022-04-13 00:52:48 -0700

    Add project files.

---
+++ ListSub/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ListSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo("C://essay1.txt");
+            //file.Attributes = FileAttributes.Normal;
+            Console.WriteLine("file name: " + file.Name);
+            Console.WriteLine("file extension: " + file.Extension);
+            Console.WriteLine("file size: " + file.Length);
+            Console.WriteLine("file creation time: " + file.CreationTime);
+            Console.WriteLine("file last access time: " + file.LastAccessTime);
+            Console.ReadKey();
+
+        }
+    }
+}
